eb2758f105d9c59190a3e1a788cb450bd0605c58
jmusila <jonathanmusila6@gmail.com>
2021-08-12 10:12:45 +0300

    added multiselect

--- app/Http/Controllers/FrontEnd/CalculatorController.php
+++ app/Http/Controllers/FrontEnd/CalculatorController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
+use App\Models\GiftCard;
 use Illuminate\Http\Request;
 
 class CalculatorController extends Controller
 {
     /**
-     * 
+     * Handle Frontend Calculator
+     *
+     * @return View
      */
+    public function index()
+    {
+        $gifts = GiftCard::all();
+
+        return view('welcome')->with('gifts', $gifts);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cart Discount Calculator</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
 </head>
 
 <body>
@@ -26,9 +27,13 @@
                         </div>
                         <div class="form-group">
                             <label for="gift_card_ids">Gift Cards</label>
-                            <select name="gift_card_ids" class="form-control">
+                            <select name="gift_card_ids[]" class="form-control" multiple>
                             <option value="">Select Gift Card</option>
-
+                                @foreach($gifts as $gift)
+                                <option value="{{ $gift->id }}">
+                                    {{ $gift->name }}
+                                </option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="form-group float-right">
@@ -41,5 +46,7 @@
         </div>
     </div>
 </body>
-
+<script>
+    $('select').selectpicker();
+</script>
 </html>
